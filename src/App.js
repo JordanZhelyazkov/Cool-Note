7af87ec518d5a6c293c900f3a3a50f8a1d8a0fb5
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-08 23:59:33 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ function App() {
 let [text, handleChange] = useState("");
 
  const resetTxt = () => {
-  handleChange('')
+  handleChange(text);
 };
 
  const saveTxt = () => {
    handleChange(text)
   localStorage.setItem('txt', text);
  }
+
+
  
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea  value={text}  onChange={(e) => handleChange(e.target.value)} className="textarea is-large" placeholder="Notes..." />
+            <textarea value={text || localStorage.getItem('txt')} onChange={(e) => handleChange(e.target.value)} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
-        <button type="submit" onClick={saveTxt} className="button is-large is-primary is-active">Save</button>
-        <button type="reset" onClick={resetTxt} className="button is-large">Clear</button>
+        <button  onClick={saveTxt}  className="button is-large is-primary is-active">Save</button>
+        <button  onClick={resetTxt} className="button is-large">Clear</button>
       </div>
     </div>
   );
