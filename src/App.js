e0a71ac57d80a4534cc4a57a5d777ba6d729044c
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-08 14:43:21 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+const [text, handleChange] = useState("");
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea value={text} onChange={(e) => handleChange(e.target.value)} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
         <button className="button is-large is-primary is-active">Save</button>
