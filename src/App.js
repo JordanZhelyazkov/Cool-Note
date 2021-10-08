5d54554b29800e5407b061f967efc903e1b628f5
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-08 16:24:20 +0300

    fixes on localStorage

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const [text, handleChange] = useState("");
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea value={text}  onChange={(e) => handleChange(e.target.value)} className="textarea is-large" placeholder="Notes..." />
+            <textarea value={text || ""}  onChange={(e) => handleChange(e.target.value)} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
         <button type="submit" onClick={saveTxt} className="button is-large is-primary is-active">Save</button>
